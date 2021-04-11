54c1c213d4e4b7dafdcff56864a1f377fc464d59
nagytommy76 <nagytommy76@gmail.com>
2021-04-11 13:29:50 +0200

    Fixed transition errors

--- src/components/BaseComponents/BaseModal/BaseModal.jsx
+++ src/components/BaseComponents/BaseModal/BaseModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef }  from 'react'
+import React, { useRef, lazy }  from 'react'
 import { connect } from 'react-redux'
 import { hideModal } from '../../../store/Actions/Modal'
 
@@ -6,48 +6,31 @@ import PropTypes from 'prop-types'
 import classes from './BaseModal.module.css'
 import { CSSTransition } from 'react-transition-group';
 
+// import Background from './ModalBackground/Background'
+const ModalBackground = lazy(() => import(/* webapckChunkName: "ModalBackground" */'./ModalBackground/Background'))
 
 const BaseModal = ({ isModalOpen, closeModal, children }) => {
     const nodeRef = useRef(null)
     return (
         <>
-            <CSSTransition 
-                nodeRef={nodeRef}
-                in={isModalOpen}
-                timeout={1000} 
-                mountOnEnter           
-                unmountOnExit 
-                classNames={{
-                    enter: classes.BackgroundEnter,
-                    enterActive: classes.BackgroundEnterActive,
-                    // enterDone: classes.exit,
-                    exit: classes.exit,
-                    exitActive: classes.exitActive,
-                    // exitDone: classes.exitDone
-                }}
-            >
-                <section className={classes.Background} onClick={closeModal}></section>
-            </CSSTransition>
-            {/* <CSSTransition
+            <ModalBackground />
+            <CSSTransition
                 nodeRef={nodeRef}
                 in={isModalOpen}
                 timeout={300}
                 mountOnEnter
                 unmountOnExit             
                 classNames={{
-                    appear: classes.modalEnter,
                     enter: classes.modalEnter,
                     enterActive: classes.modalEnterActive,
-                    enterDone: classes.modalEntered,
                     exit: classes.modalExit,
-                    exitActive: classes.modalExitActive,  
-                    exitDone: classes.modalExitDone  
+                    exitActive: classes.modalExitActive,   
                 }}
             >
                 <div className={classes.Modal} ref={nodeRef}>
                     {children}
                 </div>
-            </CSSTransition> */}
+            </CSSTransition>
         </>
     )
 }
---
+++ src/components/BaseComponents/BaseModal/ModalBackground/Background.jsx
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react'
+import { connect } from 'react-redux'
+import { hideModal } from '../../../../store/Actions/Modal'
+
+import { CSSTransition } from 'react-transition-group';
+import bgClasses from './Background.module.css'
+
+const Background = ({ isModalOpen, closeModal }) => {
+    const nodeRef = useRef(null)
+    return (
+        <CSSTransition
+            nodeRef={nodeRef}
+            in={isModalOpen}
+            timeout={600} 
+            mountOnEnter           
+            unmountOnExit 
+            classNames={{
+                enter: bgClasses.BackgroundEnter,
+                enterActive: bgClasses.BackgroundEnterActive,
+                exit: bgClasses.BackgroundExit,
+                exitActive: bgClasses.BackgroundExitActive,
+            }}
+        >
+            <section ref={nodeRef} className={bgClasses.Background} onClick={closeModal}></section>
+        </CSSTransition>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        isModalOpen: state.modalReducer.isModalOpen
+    }
+}
+
+const mapDipatchToProps = dispatch => {
+    return{
+        closeModal: () => dispatch(hideModal())
+    }
+}
+
+export default connect(mapStateToProps, mapDipatchToProps)(Background)

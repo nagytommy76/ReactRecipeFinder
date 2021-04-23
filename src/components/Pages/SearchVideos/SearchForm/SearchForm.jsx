37347b37bf97122128a8c374a1c2b7917fe157ea
nagytommy76 <nagytommy76@gmail.com>
2021-04-23 20:17:51 +0200

    Separated SearchForm

---
+++ src/components/Pages/SearchVideos/SearchForm/SearchForm.jsx
@@ -0,0 +1,54 @@
+import { useState, lazy } from 'react'
+import { connect } from 'react-redux'
+import { getVideosBySearchParameters } from '../../../../store/Actions/Video'
+
+import { Form, Title } from '../../SearchRecipes/Includes/SearchSection/StyledSearch'
+
+const BaseInput = lazy(() => import(/* webpackChunkName: "BaseInput" */ '../../../BaseComponents/BaseInputs/BaseInput'))
+
+const SearchForm = ({ getVideos, isLightTheme }) => {
+    const [videoName, setVideoName] = useState('')
+    const [numberPerPage, setNumberPerPage] = useState(10)
+
+    const sendRequest = event => {
+        event.preventDefault()
+        getVideos({
+            videoName,
+            numberPerPage
+        })
+        console.log(videoName)
+        console.log(numberPerPage)
+    }
+
+    return (
+        <Form lightTheme={isLightTheme} onSubmit={sendRequest}>
+            <Title>Search Videos</Title>
+            <BaseInput 
+                labelText="Video name:"
+                value={videoName}
+                eventOnChange={event => setVideoName(event.target.value)}
+            />
+            <BaseInput 
+                labelText="Results per page"
+                value={numberPerPage}
+                inputType="number"
+                eventOnChange={event => setNumberPerPage(event.target.value)}
+            />
+            <button>Teszt</button>
+        </Form>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        isLightTheme: state.themeReducer.isLightTheme
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getVideos: (payload) => dispatch(getVideosBySearchParameters(payload))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchForm)
--- src/components/Pages/SearchVideos/SearchVideos.jsx
+++ src/components/Pages/SearchVideos/SearchVideos.jsx
@@ -1,23 +1,14 @@
-import { connect } from 'react-redux'
-import { getVideosBySearchParameters } from '../../../store/Actions/Video'
+import { lazy } from 'react'
 
-const SearchVideos = ({ getVideos }) => {
+const SearchForm = lazy(() => import(/* webpackChunkName: "SearchForm" */ './SearchForm/SearchForm'))
+
+const SearchVideos = () => {
     return (
-        <div>
-            <h1>Search Videos</h1>
-            <button onClick={getVideos}>Teszt</button>
-        </div>
+        <section>
+            <SearchForm/>
+        </section>
     )
 }
 
+export default SearchVideos
 
-const mapDispatchToProps = dispatch => {
-    return {
-        getVideos: () => dispatch(getVideosBySearchParameters({
-            videoName: 'pasta',
-            numberPerPage: 2
-        }))
-    }
-}
-
-export default connect(null, mapDispatchToProps)(SearchVideos)
